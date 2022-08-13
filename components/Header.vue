7b88704453fdbef56a343081c99a6f7db94af4a6
chetannn <chetankharel7@gmail.com>
2022-08-13 20:17:08 +0545

    refactored theme switcher to its component

--- components/Header.vue
+++ components/Header.vue
@@ -1,12 +1,3 @@
-<script setup>
-  import {  SunIcon } from '@heroicons/vue/outline/index.js'
-  import { MoonIcon } from '@heroicons/vue/outline/index.js'
-
-  const colorMode = useColorMode()
-  const onClick = () => (colorMode.value === 'light' ? (colorMode.preference = 'dark') : (colorMode.preference = 'light'))
-
-</script>
-
 <template>
       <header class="flex flex-col items-center md:justify-between md:flex-row dark:text-white">
 
@@ -17,17 +8,7 @@
   <nav class="flex flex-col items-center mt-6 md:mt-0 md:flex-row md:justify-between md:gap-10 font-bold text-lg">
     <CustomNuxtLink to="/blogs" class="mb-6 block md:mb-0">Thoughts</CustomNuxtLink>
     <CustomNuxtLink to="/uses" class="mb-6 block md:mb-0">Uses</CustomNuxtLink>
-
-    <button @click="onClick" class="block md:mb-0" aria-label="Color Mode">
-      <ColorScheme placeholder="..." tag="span">
-    <MoonIcon v-if="colorMode.value === 'dark'" class="h-5 w-5 text-gray-500 dark:text-white" />
-    <SunIcon v-else class="h-5 w-5 text-gray-500 dark:text-white" />
-    </ColorScheme>
-
-    </button>
-
-    
-
+    <ThemeSwitcher class="block md:mb-0" />
   </nav>
   </header>
 </template>
---
+++ components/ThemeSwitcher.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import {  SunIcon } from '@heroicons/vue/outline/index.js'
+  import { MoonIcon } from '@heroicons/vue/outline/index.js'
+
+  const colorMode = useColorMode()
+  const onClick = () => (colorMode.value === 'light' ? (colorMode.preference = 'dark') : (colorMode.preference = 'light'))
+</script>
+
+<template>
+ <button @click="onClick" aria-label="Color Mode">
+      <ColorScheme placeholder="..." tag="span">
+    <MoonIcon v-if="colorMode.value === 'dark'" class="h-5 w-5 text-gray-500 dark:text-white" />
+    <SunIcon v-else class="h-5 w-5 text-gray-500 dark:text-white" />
+    </ColorScheme>
+    </button>
+</template>
